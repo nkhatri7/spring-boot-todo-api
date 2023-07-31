7551d5c6151ab4c80e8cfdb972fe7c886de7f11c
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-07-31 22:39:23 +1000

    feat: add task model

---
+++ src/main/java/com/example/todo/task/Task.java
@@ -0,0 +1,78 @@
+package com.example.todo.task;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "Task")
+@Table(name = "task")
+public class Task {
+    @Id
+    @SequenceGenerator(
+            name = "task_sequence",
+            sequenceName = "task_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "task_sequence"
+    )
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    @Column(nullable = false)
+    private LocalDate dueDate;
+    @Column(nullable = false)
+    private boolean completed;
+
+    public Task() {}
+
+    public Task(String title, String description, LocalDate dueDate,
+                boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
